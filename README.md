b41be1c762e20b32801f074b74c77aa6b8ed5c92
Björn Winterfjord <bjorn.winterfjord@iths.se>
2024-10-07 20:22:46 +0200

    Add README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # linear
+
+### G-uppgiften
+G-uppgiften (deluppgifterna c-f) finns i filen __sort.py__. Här läses de 600 punkterna från filen __unlabelled_data.csv__ in, och kategoriseras genom att undersöka om varje punkt ligger ovanför eller under linjen $y=-1.5x+0.2$ (denna linje har valts genom en okulär undersökning av en plot av samtliga punkter).
+
+Varje punkt får ett tillägg i form av siffran 0 (under/vänster om linjen) eller 1 (över/höger om linjen). Dessa data skrivs sedan till filen __labelled_data.csv__.
+
+Vi plottar också samtliga punkter tillsammans med den föreslagna linjen, så att man kan få en uppfattning om den verkar rimlig. Punkterna har färg efter sin kategori. För att förenkla plottandet har punkterna delats upp i separata listor, efter klasstillhörighet. 
+
+### VG-uppgiften
+VG-uppgiften återfinns i notebooken __sort_alternative.ipynb__. (_OBS! Viktigt att köra python-fönstren uppifrån och ned, då vissa variabelnamn definierats i tidigare fönster!_) Här undersöks några alternativ till den föreslagna linjen från G-uppgiften, och modellernas rimlighet diskuteras.
+
+Alla linjer plottas först i samma fönster, tillsammans med punkterna (ännu oklassificerade), för att man enklast ska kunna jämföra dem. Därefter följer en fyrfaldig klassificering av punkterna, baserat på var och en av de fyra linjerna. All klassificering (klass 0 resp. klass 1) sparas i fyra separata listor, tillsammans med de ursprungliga punkterna, för eventuell framtida referens. (Däremot skrivs inga data till någon ny fil.)
+
+För var och en av linjerna räknas anatalet punkter i klass 0 respektive klass 1. Det visar sig bli mycket jämna uppdelningar, dock aldrig 300-300. Separata plottar för var och en av de fyra linjerna (nu med olikfärgade klassificerade punkter) ritas upp och jämförs i den sammanfattande diskussionen.
--- sort.py
+++ sort.py
@@ -8,28 +8,28 @@ k = -1.5                # k=slope
 m = 0.2                 # m=intersect
 
 data_list=[]            # all points will be stored here
-x0, y0, x1, y1 = [], [], [], []     # separate lists for easier plotting
+x0, y0, x1, y1 = [], [], [], []     # separate lists for easier plotting. x0 and y0 are lists of coordinates of points in class 0, and vice versa for class 1.
 
 def read_unlabelled_data():
     with open(path1, "r") as data:
         for row in data:
             row_list=row.strip().split(",")
-            data_list.append(row_list)
+            data_list.append(row_list)     # all points are stored in the nested list data_list
     
-    for j in data_list:     # each datapoint is appended with 0 (below line) or 1 (above line)
+    for j in data_list:     # each datapoint is appended with 0 (below/left of line) or 1 (above/right of line)
         x=float(j[0])
         y=float(j[1])
-        if y>(k * x + m):
+        if y > (k * x + m):   
             j.append(1)  
         else:
             j.append(0)
 
-def plot_line():
+def plot_line():            # the proposed line is plotted
     x_values=np.linspace(-5,5,11)
     y_values=[k * x + m for x in x_values]
     plt.plot(x_values, y_values)
 
-def make_lists_by_classes():
+def make_lists_by_classes():    # separate lists of coordinates for class 0 and class 1 respectively. For plotting.
     class_0 = [row for row in data_list if row[2]==0]
     class_1 = [row for row in data_list if row[2]==1]    
     for j in class_0:
@@ -39,17 +39,17 @@ def make_lists_by_classes():
         x1.append(float(j[0]))
         y1.append(float(j[1]))
 
-def plot_points():
-    plt.scatter(x0, y0, color="b")
-    plt.scatter(x1, y1, color="r")
-    plt.legend(["Class 0", "Class 1", f"y={k}x+{m}"], loc="upper left")
+def plot_points():              # Class 0 points are plotted in blue, class 1 in red.
+    plt.scatter(x0, y0, color="b", s=6)
+    plt.scatter(x1, y1, color="r", s=6)
+    plt.legend([f"y={k}x+{m}", "Class 0", "Class 1"], loc="upper left")
     plt.xlim(-5,5)
     plt.ylim(-5,5)
     plt.grid()
     plt.show()
 
 
-def write_labelled_data():
+def write_labelled_data():      # classified points are written to labelled_data.csv
     with open(path2, "w", newline='') as labelled:
         for row in data_list:
             labelled.write(f"{row[0]}, {row[1]}, {row[2]}\n")
